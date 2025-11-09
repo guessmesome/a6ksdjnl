431fed43dbda90327fa2db8a9eee14ad6f2a8dd6
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-09 15:36:04 +0700

    upd

---
+++ config.js
@@ -0,0 +1,7 @@
+const productConfig = {
+    product1: 'https://trnsfx.ru/go/sxy78fcmrg',
+    product2: 'https://trnsfx.ru/go/s9vu4qnk9g',
+    product3: 'https://trnsfx.ru/go/snggyf0n7o',
+    product4: 'https://trnsfx.ru/go/sajc11nfi6'
+};
+
